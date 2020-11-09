6e164976552ec6af243a0ab32478775d93399447
Aidan Duffy <aduffy6144@gmail.com>
2020-11-09 18:06:59 -0500

    Finished module1 lectures and exercises

--- CS521/Notes/Lecture/Module1Exercises.py
+++ CS521/Notes/Lecture/Module1Exercises.py
@@ -14,3 +14,30 @@ farenheit = input("What is the temperature in degrees Farenheit? ")
 farenheit = float(farenheit)
 celsius = (5/9)*(farenheit-32)
 print("It is ", celsius, " degrees Celsius")
+
+def print_1 (x):
+	print (x)
+x = 'morning '
+print_1 (x) #morning
+
+def print_2 (x):
+	x = 'evening '
+	print (x)
+x = 'morning '
+print_2 (x) #evening
+
+x = 0
+y = 0
+z = 10
+if z > 25:
+	x = z**2
+	y = z**3
+print (x, y) #0 0
+
+x = 0
+y = 0
+z = 10
+if z > 25:
+	x = z **2
+y = z**3
+print (x, y) #0, 1000
