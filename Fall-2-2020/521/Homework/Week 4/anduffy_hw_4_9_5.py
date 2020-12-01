dfa2d2ff28b4732b28d772b8ae1ae478d1b2d92d
Aidan Duffy <aduffy6144@gmail.com>
2020-12-01 15:35:46 -0500

    Finished 521 HW5 prob5

--- Fall-2-2020/521/Homework/Week 4/anduffy_hw_4_9_5.py
+++ Fall-2-2020/521/Homework/Week 4/anduffy_hw_4_9_5.py
@@ -2,35 +2,53 @@
 Aidan Duffy
 Class: CS 521 - Fall 2
 Date: December 1, 2020
-Homework Problem 4.9.4
-Description: This prints keys, values, key-value pairs, pairs in order of key as well as
-in order of value.
+Homework Problem 4.9.5
+Description: This program creates a dictionary with letter frequency, a string of the most common letter(s)
+and a histogram of letter frequencies.
 """
 
-def print_dict(my_dict):
+def sentence_word_freq(sentence):
     """
-    This prints keys, values, key-value pairs, pairs in order of key as well as
-    in order of value.
-    :param my_dict: is the input dictionary.
+    This program creates a dictionary with letter frequency, a string of the most common letter(s)
+    and a histogram of letter frequencies.
+    :param sentence: is the input sentence.
     :return:
     """
-    keys = str(my_dict.keys())
-    vals = str(my_dict.values())
-    key_val_pairs = str(my_dict.items())
-    key_val_pairs_sorted_keys = str(sorted(my_dict.items()))
-    key_val_pairs_sorted_vals = ""
-    for val in sorted(my_dict, key = my_dict.get):
-        key_val_pairs_sorted_vals += (str(val) + ": " + str(my_dict[val])) + ", "
-    #The reformatting below just makes the print statements more readable
-    print("a. Keys: ", keys[10:len(keys)-1])
-    print("b. Values: ", vals[13:len(vals)-2])
-    print("c. Key value pairs: ", key_val_pairs[11:len(key_val_pairs)-1])
-    print("d. Key value pairs ordered by key: ", key_val_pairs_sorted_keys)
-    print("e. Key value pairs ordered by val: ", key_val_pairs_sorted_vals[:len(key_val_pairs_sorted_vals)-2])
+    print("The string being analyzed is: ", sentence)
+    sentence_dict = dict()
+    max_frequency = 0
+    max_frequency_list = []
+    for char in sentence:
+        if char == ' ':
+            continue
+        if char in sentence_dict:
+            sentence_dict[char] += 1
+            if sentence_dict[char] > max_frequency:
+                max_frequency = sentence_dict[char]
+                max_frequency_list = list()
+                max_frequency_list.append(char)
+            elif sentence_dict[char] == max_frequency:
+                max_frequency_list.append(char)
+        else:
+            sentence_dict[char] = 1
+    print("1. Sorted dictionary of letter counts: ", sorted(sentence_dict.items()))
+    if len(max_frequency_list) == 1:
+        print("2. Most frequent letter \"", max_frequency_list[0], "\" appears ", max_frequency, " times.", sep='')
+    else:
+        print("2. Most frequent letters", max_frequency_list, "appears", max_frequency, "times.")
+    print("3. Histogram:")
+    for key in sorted(sentence_dict.keys()):
+        print(key*sentence_dict[key])
 
 def main():
-    my_dict = {'a':15,'c':18,'b':20}
-    print_dict(my_dict)
+    sentence = str(input("Please input a sentence that is at least 15 characters long: ")).upper()
+    if len(sentence) < 15:
+        raise Exception("Please enter a sentence with more than 15 characters!")
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for char in sentence:
+        if char not in alpha and char != ' ':
+            raise Exception("Please only input letters! Try again")
+    sentence_word_freq(sentence)
 
 if __name__ == "__main__":
     main()
