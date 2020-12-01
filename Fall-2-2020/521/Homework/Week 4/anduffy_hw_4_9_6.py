031f64872de2e22413284c69fd823b73350f9566
Aidan Duffy <aduffy6144@gmail.com>
2020-12-01 15:47:36 -0500

    Finished 521 HW4 prob 6, so everything, need to compress

--- Fall-2-2020/521/Homework/Week 4/anduffy_hw_4_9_6.py
+++ Fall-2-2020/521/Homework/Week 4/anduffy_hw_4_9_6.py
@@ -3,25 +3,36 @@ Aidan Duffy
 Class: CS 521 - Fall 2
 Date: December 1, 2020
 Homework Problem 4.9.6
-Description: This creates a dictionary of first (V) and last (K) names.
+Description: Gets a number from a user, converts the number to words using a dictionary and prints that.
 """
 
-def name_dictionary(first_names,last_names):
+def number_as_word(number):
     """
-    Takes in lists of first and last names, then creates a dictionary from that.
-    :param first_names: This is the list of all the first names
-    :param last_names: This is the list of all the last names
+    Gets a number from a user, converts the number to words using a dictionary and prints that.
+    :param number: is the input provided by the user.
     :return:
     """
-    name_dict = dict(zip(last_names,first_names))
-    print("First Names: ", first_names)
-    print("Last Names: ", last_names)
-    print("Name Dictionary: ", name_dict)
+    num_dict = {".":"Point", "0":"Zero", "1":'One', "2":'Two',
+                "3":"Three","4":"Four","5":"Five","6":"Six",
+                "7":"Seven","8":"Eight","9":"Nine", "-":"Negative"}
+    output = "As Text:"
+    for digit in number:
+        output += " " + num_dict[digit]
+    print(output)
 
 def main():
-    first_names = ['Jane','John','Jack','Will']
-    last_names = ['Doe','Deere','Black','Ferrell']
-    name_dictionary(first_names,last_names)
+    given_number = False
+    while given_number is False:
+        try:
+            number = (input("Enter a number: "))
+            if ',' in number:
+                print("Please try again without entering commas.")
+            else:
+                number_as_int = float(number)
+                given_number = True
+                number_as_word(number)
+        except:
+            print("Please enter a valid number! \"", number, "\" is not a valid number.", sep='')
 
 if __name__ == "__main__":
     main()
