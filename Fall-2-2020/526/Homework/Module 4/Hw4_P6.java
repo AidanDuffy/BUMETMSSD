d54fba7338ee7d8db9b06ac2f2ac4e13dcf2ea51
Aidan Duffy <aduffy6144@gmail.com>
2020-11-30 22:47:29 -0500

    Finished HW4 for 526, need to compress and submit

--- Fall-2-2020/526/Homework/Module 4/Hw4_P6.java
+++ Fall-2-2020/526/Homework/Module 4/Hw4_P6.java
@@ -1,65 +1,94 @@
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class Hw4_P6 {
 
     public static void main(String[] args) {
-        System.out.println("HI");
+        final int NUM_KEYS = 100000; //For all intents and purposes, this is the "N" for any big-Oh analysis.
+
+        HashMap myMap = new HashMap();
+        ArrayList myArrayList = new ArrayList();
+        LinkedList myLinkedList = new LinkedList();
+
+        long startTime, endTime, elapsedTime;
+
+        Random r = new Random();
+
+        int[] insertKeys = new int[NUM_KEYS];
+        int[] searchKeys = new int[NUM_KEYS];
+
         /**
-         * create a HashMap instance myMap
-         * create an ArrayList instance myArrayList
-         * create a LinkedList instance myLinkedList
-         * Repeat the following 10 times and calculate average total insertion time and average total
-         * search time for each data structure
-         * generate 100,000 random integers in the range [1, 1,000,000] and store them in the
-         * array of integers insertKeys[ ]
-         * // begin with empty myHap, myArrayList, and myLinkedList each time
-         * // Insert keys one at a time but measure only the total time (not individual insert
-         * // time)
-         * // Use put method for HashMap
-         * // Use add method for ArrayList and LinkedList
-         * insert all keys in insertKeys [ ] into myMap and measure the total insert time
-         * insert all keys in insertKeys [ ] into  myArrayList and measure the total insert time
-         * insert all keys in insertKeys [ ] into myLinkedList and measure the total insert time
-         * generate 100,000 random integers in the range [1, 2,000,000] and store them in the
-         * array searchKeys[ ].
-         * // Search keys one at a time but measure only total time (not individual search
-         * // time)
-         * // Use containsKey method for HashMap
-         * // Use contains method for ArrayList and Linked List
-         * search myMap for all keys in searchKeys[ ] and measure the total search time
-         * search myArrayList for all keys in searchKeys[ ] and measure the total search time
-         * search myLinkedList for all keys in searchKeys[ ] and measure the total search time
-         * Print your output on the screen using the following format:
-         * Number of keys = 100000
-         * HashMap average total insert time = xxxxx
-         * ArrayList average total insert time = xxxxx
-         * LinkedList average total insert time = xxxxx
-         * HashMap average total search time = xxxxx
-         * ArrayList average total search time = xxxxx
-         * LinkedList average total search time = xxxxx
-         * You can generate n random integers between 1 and N in the following way:
-         * Random r = new Random(System.currentTimeMillis() );
-         * for i = 0 to n – 1
-         *     a[i] = r.nextInt(N) + 1
-         * When you generate random numbers, it is a good practice to reset the seed. When you first create
-         * an instance of the Random class, you can pass a seed as an argument, as shown below:
-         * Random r = new Random(System.currentTimeMillis());
-         * You can pass any long integer as an argument. The above example uses the current time as a
-         * seed.
-         * Later, when you want to generate another sequence of random numbers using the same Random
-         * instance, you can reset the seed as follows:
-         *  r.setSeed(System.currentTimeMillis());
-         * You can also use the Math.random( ) method. Refer to a Java tutorial or reference manual on
-         * how to use this method.
-         * We cannot accurately measure the execution time of a code segment. However, we can estimate
-         * it by measuring an elapsed time, as shown below:
-         *  long startTime, endTime, elapsedTime;
-         * startTime = System.currentTimeMillis();
-         * // code segment
-         * endTime = System.currentTimeMillis();
-         * elapsedTime = endTime ‐ startTime;
-         * We can use the elapsedTime as an estimate of the execution time of the code segment. 
+         * These two double arrays will keep a running average of the insert/search times for the 3 data structures.
+         * [0] will be the hash map, [1] will be the array list, and [2] will be the linked list.
          */
+        double[] insertTimes = new double[3];
+        double[] searchTimes = new double[3];
+
+        for (int i = 0; i < 10; i += 1) {
+            r.setSeed(System.currentTimeMillis());
+            for (int j = 0; j < NUM_KEYS; j += 1) {
+                insertKeys[j] = r.nextInt(1000000) + 1;
+                searchKeys[j] = r.nextInt(2000000) + 1;
+            }
+            /**
+             * This outer loop allowed me to keep the code compact and succinct. Otherwise, I would have needed
+             * to write the inner for loops six times, two for each type of data structure.
+             */
+            for (int dataStruct = 0; dataStruct < 3; dataStruct += 1) {
+                /**
+                 * The following for loop provides an estimate of insert time of 100K elements
+                 * into the various data structures.
+                 */
+                startTime = System.currentTimeMillis();
+                for (int h = 0; h < NUM_KEYS; h += 1) {
+                    if (dataStruct == 0) { //HashMap
+                        myMap.put(insertKeys[h],i);
+                    } else if (dataStruct == 1) { //ArrayList
+                        myArrayList.add(insertKeys[h]);
+                    } else { //LinkedList
+                        myLinkedList.add(insertKeys[h]);
+                    }
+
+                }
+                endTime = System.currentTimeMillis();
+                elapsedTime = endTime - startTime;
+                insertTimes[dataStruct] = (insertTimes[dataStruct]*i + elapsedTime)/(i+1);
+                /**
+                 * The following for loop provides an estimate of search time of 100K elements
+                 * into the various data structures.
+                 */
+                startTime = System.currentTimeMillis();
+                for (int h = 0; h < NUM_KEYS; h += 1) {
+                    if (dataStruct == 0) { //HashMap
+                        myMap.containsKey(searchKeys[h]);
+                    } else if (dataStruct == 1) { //ArrayList
+                        myArrayList.contains(searchKeys[h]);
+                    } else { //LinkedList
+                        myLinkedList.contains(searchKeys[h]);
+                    }
+
+                }
+                endTime = System.currentTimeMillis();
+                elapsedTime = endTime - startTime;
+                searchTimes[dataStruct] = (searchTimes[dataStruct]*i + elapsedTime)/(i+1);
+            }
+            myMap = new HashMap();
+            myArrayList = new ArrayList();
+            myLinkedList = new LinkedList();
+        }
+        /**
+         * The rest of the program will be printing out the observations.
+         */
+
+        System.out.println("Number of keys = " + String.valueOf(NUM_KEYS) + "\n");
+        System.out.println("HashMap average total insert time = " + String.valueOf(insertTimes[0]));
+        System.out.println("ArrayList average total insert time = " + String.valueOf(insertTimes[1]));
+        System.out.println("LinkedList average total insert time = " + String.valueOf(insertTimes[2]) + "\n");
+        System.out.println("HashMap average total search time = " + String.valueOf(searchTimes[0]));
+        System.out.println("ArrayList average total search time = " + String.valueOf(searchTimes[1]));
+        System.out.println("LinkedList average total search time = " + String.valueOf(searchTimes[2]));
     }
 
 }
