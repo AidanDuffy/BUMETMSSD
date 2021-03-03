61d14a8a9208016277d1a00d088fe245dc65717c
Aidan Duffy <aduffy6144@gmail.com>
2021-03-02 19:52:20 -0500

    Quick fix

--- Spring-1-2021/622/Project/src/main/FinFree.java
+++ Spring-1-2021/622/Project/src/main/FinFree.java
@@ -12,7 +12,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class FinFree {
 
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Whatcdptpbtn44%"; //I just got rid of this since it is a password I actually use sometimes...
+    private static final String PASSWORD = "X"; //I just got rid of this since it is a password I actually use sometimes...
     private static final String CONN_STRING = "jdbc:mysql://localhost:3306/finfree";
     static Connection connection = null;
     static ReentrantLock accLock = new ReentrantLock();
