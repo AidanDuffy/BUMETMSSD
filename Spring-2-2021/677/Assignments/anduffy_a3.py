a91a5949089ea3198cd43545851056774bcbe8a5
Aidan Duffy <aduffy6144@gmail.com>
2021-04-05 11:19:18 -0400

    Skeleton for assignment 3, added notes for the first three modules

---
+++ Spring-2-2021/677/Assignments/anduffy_a3.py
@@ -0,0 +1,40 @@
+"""
+Aidan Duffy
+Class: CS 677 - Spring 2
+Date: 04/06/2021
+Homework Assignment #2
+Description of Problem (just a 1-2 line summary!): This is the program file
+containing all methods for assignment 3.
+"""
+
+
+def question1():
+    return
+
+
+def question2():
+    return
+
+
+def question3():
+    return
+
+
+def question4():
+    return
+
+
+def question5():
+    return
+
+
+def question6():
+    return
+
+
+def main():
+    print(5)
+
+
+if __name__ == '__main__':
+    main()
