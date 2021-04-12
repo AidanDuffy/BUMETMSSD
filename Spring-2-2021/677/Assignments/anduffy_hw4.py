dad4519535dd7887da5bf49fae1672dfcff9fed6
Aidan Duffy <aduffy6144@gmail.com>
2021-04-12 19:32:55 -0400

    Finished HW4

--- Spring-2-2021/677/Assignments/anduffy_hw4.py
+++ Spring-2-2021/677/Assignments/anduffy_hw4.py
@@ -8,6 +8,7 @@ containing all methods for assignment 4.
 """
 import os
 
+import matplotlib
 import pandas
 import pandas as pd
 import math
@@ -15,6 +16,7 @@ import seaborn
 from sklearn . preprocessing import StandardScaler, LabelEncoder
 from sklearn . neighbors import KNeighborsClassifier
 from sklearn . model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
@@ -37,12 +39,60 @@ def question1(data):
 
 def question2(data):
     #Group 3: X: serum sodium, Y: serum creatinine
-    df_0 = data[data['DEATH_EVENT'] == 0]
-    df_0 = df_0[['serum_sodium','serum_creatinine']]
+    X = np.array(data['serum_sodium'])
+    Y = np.array(data['serum_creatinine'])
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.5,
+                                                        random_state=100)
+    for deg in range(1,4):
+        weights = np.polyfit(X_train,Y_train,deg)
+        model = np.poly1d(weights)
+        predicted = model(X_test)
+        rmse = np.sqrt(mean_squared_error(Y_test,predicted))
+        r2 = r2_score(Y_test,predicted)
+        print("For polynomial of degree " + "{:.0f}".format(deg))
+        weight_str = ""
+        for w in weights:
+            weight_str += "{:.2f}".format(w) + ", "
+        print("Weights: " + weight_str)
+        print("R Mean Squared Error: " + "{:.2f}".format(rmse))
+        print("R2: " + "{:.2f}".format(r2))
+        matplotlib.pyplot.scatter(X_test,Y_test,c='green')
+        matplotlib.pyplot.scatter(X_test, predicted, c='red')
+        matplotlib.pyplot.show()
+        loss = 0
+        for i in range(len(Y_test)):
+            loss += (Y_test[i] - predicted[i])**2
+        print("Estimated Loss: " + "{:.2f}".format(loss) + "\n\n")
+    question2_logs(X_train, X_test, Y_train, Y_test)
     return
 
 
-def question3(data):
+def question2_logs(X_train, X_test, Y_train, Y_test):
+    deg = 1
+    weights = [np.polyfit(np.log(X_train), Y_train, deg),
+               np.polyfit(np.log(X_train), np.log(Y_train), deg)]
+    for weight in weights:
+        model = np.poly1d(weight)
+        predicted = model(np.log(X_test))
+        if deg == 2:
+            Y_test = np.log(Y_test)
+        rmse = np.sqrt(mean_squared_error(Y_test, predicted))
+        r2 = r2_score(Y_test, predicted)
+        print("For logarithm (type " + str(deg) + "):")
+        deg += 1
+        weight_str = ""
+        for w in weight:
+            weight_str += "{:.2f}".format(w) + ", "
+        print("Weights: " + weight_str)
+        print("R Mean Squared Error: " + "{:.2f}".format(rmse))
+        print("R2: " + "{:.2f}".format(r2))
+        matplotlib.pyplot.scatter(X_test, Y_test, c='green')
+        matplotlib.pyplot.scatter(X_test, predicted, c='red')
+        matplotlib.pyplot.show()
+        loss = 0
+        for i in range(len(Y_test)):
+            loss += (Y_test[i] - predicted[i]) ** 2
+        print("Estimated Loss: " + "{:.2f}".format(loss) + "\n\n")
     return
 
 
@@ -54,8 +104,11 @@ def main():
     data = pandas.read_csv(data_file)
     data = data[['creatinine_phosphokinase', 'serum_creatinine', 'serum_sodium',
                 'platelets','DEATH_EVENT']]
-    question1(data)
-    question2(data)
+    #question1(data)
+    print("For survivors:\n")
+    question2(data[data['DEATH_EVENT'] == 0]) #Survivors
+    print("=======================\nFor deceased:\n")
+    question2(data[data['DEATH_EVENT'] == 1]) #Deceased
 
 
 
